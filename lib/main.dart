fae52d1bcb92d8aa67136a9703a49d28a2ee7e1e
AlexE <alexander.edwards3@ontariotechu.net>
2021-10-05 17:35:44 -0400

    Added new model

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,11 @@ class MyApp extends StatelessWidget {
     ]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Detect Room Color',
+      title: 'Garbage Detection Version 1.1',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      home: DetectScreen(title: 'Detect Room Color'),
+      home: DetectScreen(title: 'Garbage Detection Version 1.1'),
     );
   }
 }
