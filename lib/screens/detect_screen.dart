fe9ee59848f77afa525cdc90aea630f19cb9f4ca
AlexE <alexander.edwards3@ontariotechu.net>
2021-10-17 20:07:43 -0400

    Added geolocation and link to backend

--- lib/screens/detect_screen.dart
+++ lib/screens/detect_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -6,6 +8,8 @@ import 'package:tensorflow_lite_flutter/helpers/app_helper.dart';
 import 'package:tensorflow_lite_flutter/helpers/camera_helper.dart';
 import 'package:tensorflow_lite_flutter/helpers/tflite_helper.dart';
 import 'package:tensorflow_lite_flutter/models/result.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 
 class DetectScreen extends StatefulWidget {
   DetectScreen({Key key, this.title}) : super(key: key);
@@ -23,9 +27,50 @@ class _DetectScreenPageState extends State<DetectScreen>
 
   List<Result> outputs;
 
-  void initState() {
-    super.initState();
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
 
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // setState(() async {
+    //   var temp = await Geolocator.getCurrentPosition();
+    //   getLocation['lat'] = temp.latitude;
+    //   getLocation['lng'] = temp.longitude;
+    // });
+    return await Geolocator.getCurrentPosition();
+  }
+
+  void initState() {
+
+    Geolocator.requestPermission();
+    super.initState();
     //Load TFLite Model
     TFLiteHelper.loadModel().then((value) {
       setState(() {
@@ -35,30 +80,60 @@ class _DetectScreenPageState extends State<DetectScreen>
 
     //Initialize Camera
     CameraHelper.initializeCamera();
-
     //Setup Animation
     _setupAnimation();
 
     //Subscribe to TFLite's Classify events
-    TFLiteHelper.tfLiteResultsController.stream.listen((value) {
-      value.forEach((element) {
-        _colorAnimController.animateTo(element.confidence,
-            curve: Curves.bounceIn, duration: Duration(milliseconds: 500));
-      });
+    TFLiteHelper.tfLiteResultsController.stream.listen(
+        (value) {
+          value.forEach((element) {
+            _colorAnimController.animateTo(element.confidence,
+                curve: Curves.bounceIn, duration: Duration(milliseconds: 500));
+          });
 
-      //Set Results
-      outputs = value;
+          //Set Results
+          outputs = value;
+          print(outputs[0].label);
 
-      //Update results on screen
-      setState(() {
-        //Set bit to false to allow detection again
-        CameraHelper.isDetecting = false;
-      });
-    }, onDone: () {
+          if (outputs[0].label=='1 Garbage' && outputs[0].confidence > 0.9) {
+            print("ASDASDASDASD");
+            print(_determinePosition().then((a) =>  sendData().then((r)=>print('YE'))));
+            print(getLocation);
+            print("ASDASDASDASD");
+          }
 
-    }, onError: (error) {
-      AppHelper.log("listen", error);
-    });
+          //Update results on screen
+          setState(() {
+            //Set bit to false to allow detection again
+            CameraHelper.isDetecting = false;
+          });
+        },
+        onDone: () {},
+        onError: (error) {
+          AppHelper.log("listen", error);
+        });
+  }
+
+  var _geolocator = Geolocator();
+
+  //var _location = Location();
+  var _positionMessage = '';
+  var getLocation = {'lat': 0.0, 'lng': 0.0};
+
+  Future<http.Response> sendData() async {
+    var temp = await Geolocator.getCurrentPosition();
+
+    print("ASDKASJDKLAHSDOIASODI");
+    print(temp.longitude);
+    return http.post(Uri.parse('https://capstone-93499.web.app/api/addGarbage'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, dynamic>{
+          "geoLocation": {'lat': temp.latitude, 'lng': temp.longitude},
+          "image":   "",
+          "time": "sd"
+        }));
   }
 
   @override
